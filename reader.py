9142e2e2fd31fec189f49031b1397a2aa06038f9
Ignacio Gamboa <ign.gga@outlook.cl>
2021-08-13 15:37:00 -0400

    los archivos se guardan en formato YMD_HMS_testEVT. y los mensajes se generan en D/M/Y mensaje

--- reader.py
+++ reader.py
@@ -1,12 +1,14 @@
 import serial,os,datetime
 
 fecha=datetime.datetime.now()
-nFecha=fecha.strftime('%y%m%d_%H%M%S')
+nFecha=fecha.strftime('%Y%m%d_%H%M%S')
+mFecha=fecha.strftime('%d/%m/%Y')
 bancoEVR = serial.Serial('COM4',baudrate=9600,timeout=1)
 file = open('C:/testEVR/{}_EVRtest.txt'.format(nFecha),'a')
 
 while True:
     mBancoEVR=bancoEVR.readline()
     mensaje=str(mBancoEVR.decode('utf-8'))
-    file.write(mensaje)
-    print(mensaje)
+    
+    file.write('{} {}'.format(mFecha,mensaje))
+    print('{} {}'.format(mFecha,mensaje))
