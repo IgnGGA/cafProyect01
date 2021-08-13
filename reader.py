bbbcb7ffecc9dbed485040cf2f0979e9cd7c589b
Ignacio Gamboa <ign.gga@outlook.cl>
2021-08-13 14:47:55 -0400

    primera etapa funcional

--- reader.py
+++ reader.py
@@ -1,14 +1,8 @@
 import serial
 
-bancoEVR=serial.Serial('COM4',baudrate=9600,timeout=1)
-mBancoEVR=bancoEVR.readline()
-mensaje=str(mBancoEVR.decode('utf-8'))
+bancoEVR = serial.Serial('COM4',baudrate=9600,timeout=1)
 
 while True:
-    if (mensaje!='FIN'):
-        print(mensaje)
-    elif (mensaje==' ' or mensaje==''):
-        print(None)
-    else:
-        bancoEVR.close()
-        exit()
+    mBancoEVR=bancoEVR.readline()
+
+    print(mBancoEVR)
