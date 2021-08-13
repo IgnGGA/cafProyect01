ef41631747472a5d40eca3d98225d73f8d1119f1
Ignacio Gamboa <ign.gga@outlook.cl>
2021-08-13 15:05:51 -0400

    crea y escribe el archivo generar logica por mensajes

--- reader.py
+++ reader.py
@@ -1,8 +1,10 @@
-import serial
+import serial,os
 
 bancoEVR = serial.Serial('COM4',baudrate=9600,timeout=1)
+file = open('C:/testEVR/test.txt','a')
 
 while True:
     mBancoEVR=bancoEVR.readline()
-
-    print(mBancoEVR)
+    mensaje=str(mBancoEVR.decode('utf-8'))
+    file.write(mensaje)
+    print(mensaje)
