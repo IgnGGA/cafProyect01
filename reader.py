f933683aa4b47b7f91f4f3dd0e010f65d32d9029
Ignacio Gamboa <ign.gga@outlook.cl>
2021-08-13 14:37:32 -0400

    debiese funcionar pero algo pasa que no lo toma

--- reader.py
+++ reader.py
@@ -1,15 +1,14 @@
-import serial, datetime, os
+import serial
+
 bancoEVR=serial.Serial('COM4',baudrate=9600,timeout=1)
-fecha = datetime.datetime.now()
-nombre=str(fecha.year,fecha.month,fecha.day,fecha.hour,fecha.minute,fecha.second,'_EVRtest')
-file = open('C:/testEVR/',nombre,'.txt','a')
 mBancoEVR=bancoEVR.readline()
 mensaje=str(mBancoEVR.decode('utf-8'))
 
 while True:
-    if (mensaje!="FIN"):
-        print(fecha,mensaje)
-        file.write(fecha,mensaje)
+    if (mensaje!='FIN'):
+        print(mensaje)
+    elif (mensaje==' ' or mensaje==''):
+        print(None)
     else:
-        file.close()
         bancoEVR.close()
+        exit()
