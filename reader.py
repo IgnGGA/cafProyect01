a293546d5418672b00022551429940734f5260e2
Ignacio Gamboa <ign.gga@outlook.cl>
2021-08-16 07:39:23 -0400

    se generaran 2 archivos, uno preliminar el cual se genera por cada ciclo, y otro consolidado, que los tendra todo la ejecucion

--- reader.py
+++ reader.py
@@ -1,21 +1,40 @@
 import serial,os,datetime
 
+from serial.win32 import ERROR_INVALID_USER_BUFFER
+
 fecha=datetime.datetime.now()
 nFecha=fecha.strftime('%Y%m%d_%H%M%S')
-mFecha=fecha.strftime('%d/%m/%Y')
+mFecha=fecha.strftime('%d/%m/%y')
+ncount=1
 bancoEVR = serial.Serial('COM4',baudrate=9600,timeout=1)
-file = open('C:/testEVR/{}_EVRtest.txt'.format(nFecha),'a')
+consolidado = open('C:/testEVR/{}_EVRtest.txt'.format(nFecha),'a')
+
 
 while True:
     vacio1=''
     vacio2=' '
     finEVR='FIN'
+    offEVR='EVR: OFF'
+    onEVR='EVR: ON'
+    premilimar  = open('C:/testEVT/preliminares/{}_EVRpremilinar{}'.format(nFecha,ncount),'a')
     mBancoEVR=bancoEVR.readline()
     mensaje=str(mBancoEVR.decode('utf-8'))
+
     if (mensaje.len()>0):
-        file.write('{} {}'.format(mFecha,mensaje))
+        consolidado.write('{} {}'.format(mFecha,mensaje))
         print('{} {}'.format(mFecha,mensaje))
+        if (mensaje==onEVR):
+            premilimar  = open('C:/testEVT/preliminares/{}_EVRpremilinar{}'.format(nFecha,ncount),'a')
+        premilimar.write('{} {}'.format(mFecha,mensaje))
+
     elif (mensaje==finEVR):
+        consolidado.write('{} {}'.format(mFecha,mensaje))
         print('{} {}'.format(mFecha,mensaje))
-        file.close()
-        print('{} {}'.format(mFecha,mensaje))
+        consolidado.close()
+        bancoEVR.close()
+        exit()
+    
+    elif (mensaje==offEVR):
+        premilimar.write('{} {}'.format(mFecha,mensaje))
+        consolidado.close()
+        ncount=ncount+1
